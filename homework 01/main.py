61095b2fb0e87cf7db8bbeb478e5c0848006a9e8
Binary-Maze <myazdrikova@yandex.ru>
2021-06-22 12:39:42 +0300

    Домашнее задание 01

--- homework 01/main.py
+++ homework 01/main.py
@@ -1,32 +1,80 @@
-"""
-Домашнее задание №1
-Функции и структуры данных
-"""
+# """
+# Домашнее задание №1
+# Функции и структуры данных
+# Задача №1
+# # """
+#     функция, которая принимает N целых чисел,
+#     и возвращает список квадратов этих чисел
+#     >>> power_numbers(1, 2, 5, 7)
+#     <<< [1, 4, 25, 49]
+#     """
+#
+#
+def power_numbers(list_of_numbers):
+    list_of_numb2 = []
+    for num in list_of_numbers:
+        list_of_numb2.append(num**2)
+    return list_of_numb2
+
+numb = power_numbers([1, 2, 5, 7])
+print(numb)
 
 
-def power_numbers():
-    """
-    функция, которая принимает N целых чисел,
-    и возвращает список квадратов этих чисел
-    >>> power_numbers(1, 2, 5, 7)
-    <<< [1, 4, 25, 49]
-    """
+## Задача №2
+# # filter types
+# ODD = "odd"
+# EVEN = "even"
+# PRIME = "prime"
+#
+#
+# def filter_numbers():
+#     """
+#     функция, которая на вход принимает список из целых чисел,
+#     и возвращает только нечётные/простые числа
+#     (выбор производится передачей дополнительного аргумента)
+#
+#     >>> filter_numbers([1, 2, 3], ODD)
+#     <<< [1, 3]
+#     >>> filter_numbers([2, 3, 4, 5], EVEN)
+#     <<< [2, 4]
+#     """
+#
 
+def filter_numbers(list_of_numbers):
+    list_of_odd = []
+    for num in list_of_numbers:
+        if num % 2 != 0:
+            list_of_odd.append(num)
+    return list_of_odd
 
-# filter types
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
+numb = filter_numbers([1, 2, 3])
+print(numb)
 
+## Задача №3
+# # filter types
+# ODD = "odd"
+# EVEN = "even"
+# PRIME = "prime"
+#
+#
+# def filter_numbers():
+#     """
+#     функция, которая на вход принимает список из целых чисел,
+#     и возвращает только чётные/простые числа
+#     (выбор производится передачей дополнительного аргумента)
+#
+#     >>> filter_numbers([1, 2, 3], ODD)
+#     <<< [1, 3]
+#     >>> filter_numbers([2, 3, 4, 5], EVEN)
+#     <<< [2, 4]
+#     """
+#
+def filter_numbers(list_of_numbers):
+    list_of_even = []
+    for num in list_of_numbers:
+        if num % 2 == 0:
+            list_of_even.append(num)
+    return list_of_even
 
-def filter_numbers():
-    """
-    функция, которая на вход принимает список из целых чисел,
-    и возвращает только чётные/нечётные/простые числа
-    (выбор производится передачей дополнительного аргумента)
-
-    >>> filter_numbers([1, 2, 3], ODD)
-    <<< [1, 3]
-    >>> filter_numbers([2, 3, 4, 5], EVEN)
-    <<< [2, 4]
-    """
+numb = filter_numbers([2, 3, 4, 5])
+print(numb)
